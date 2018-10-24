e0d529c0c6d7c0b0f25038ac39b1691b0b208727
Noel Grover <159802846+noel-enquanta@users.noreply.github.com>
2018-10-24 15:50:21 -0500

    update release.sh RELEASEPATCH

--- release.sh
+++ release.sh
@@ -26,5 +26,6 @@ then
   fi
 
   version=$major'.'$minor'.'$patch
-  curl -u $GITHUBUSERNAME:$GITHUBPASSWORD -H "Content-Type: application/json" --request POST --data '{"tag_name": "'$version'", "target_commitish": "master", "name": "'$version'", "body": "", "draft": false, "prerelease": false}' https://api.github.com/repos/voiceittech/voiceit2-scala/releases
+  echo $version
+  curl -u $GITHUBUSERNAME:$GITHUBPASSWORD -H "Content-Type: application/json" --request POST --data '{"tag_name": "'$version'", "target_commitish": "master", "name": "'$version'", "body": "", "draft": false, "prerelease": false}' https://api.github.com/repos/voiceittech/VoiceIt2-Scala/releases
 fi
